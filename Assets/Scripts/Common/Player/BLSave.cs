bfcebdc1bfde7b557e455a18bcaec2781fe33a20
old-fritz <komarov.99@yandex.ru>
2019-02-13 03:24:36 +0300

    Create monitor controller

---
+++ Assets/Scripts/Common/Player/BLSave.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BLSave : MonoBehaviour
+{
+    private List<bool> choises;
+    private int choise = 0;
+        
+    private void Start()
+    {
+        choises = new List<bool>(10);
+        clear();
+    }
+
+    public void clear()
+    {
+        for (int i = 0; i < 10; i++)
+            choises[i] = false;
+        choise = 0;
+    }
+
+    public void makeChoise(int ind)
+    {
+        choises[ind-1] = true;
+        if (getBoolsCount() == 1)
+            choise = ind;
+    }
+
+    public void getEnding(out int ind, out bool result)
+    {
+        int count = getBoolsCount();
+        
+        switch (count)
+        {
+            // good all persons ending
+            case 10:
+                ind = 0;
+                result = true;
+                break;
+            // zero ending
+            case 0:
+                ind = 0;
+                result = false;
+                break;
+            // good ending
+            case 1:
+                ind = choise;
+                result = true;
+                break;
+            // bad ending
+            default:
+                ind = choise;
+                result = false;
+                break;
+        }
+    }
+
+    private int getBoolsCount()
+    {
+        int count = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (choises[i])
+                count++;
+        }
+
+        return count;
+    }
+}
--- Assets/Scripts/Scene0/MonitorController.cs
+++ Assets/Scripts/Scene0/MonitorController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonitorController : MonoBehaviour
 {
@@ -9,11 +10,101 @@ public class MonitorController : MonoBehaviour
     [SerializeField] private GameObject browserPage;
     [SerializeField] private GameObject blAlert;
     [SerializeField] private GameObject blGame;
-    
+    [SerializeField] private GameObject icons;
     
     public void openBrowser()
     {
         mainScreen.SetActive(false);
         browserSearch.SetActive(true);
+    }
+
+    public void searchBack()
+    {
+        mainScreen.SetActive(true);
+        browserSearch.SetActive(false);
+    }
+
+    public void doSearch()
+    {
+        browserSearch.SetActive(false);
+        browserPage.SetActive(true);
+    }
+
+    public void closeBrowser()
+    {
+        mainScreen.SetActive(true);
+        browserPage.SetActive(false);
+    }
+    
+
+    public void openAlert()
+    {
+        blAlert.SetActive(true);
+    }
+
+    public void closeAlert()
+    {
+        blAlert.SetActive(false);
+    }
+
+    public void openBL()
+    {
+        mainScreen.SetActive(false);
+        blAlert.SetActive(false);
+        blGame.SetActive(true);
+        
+        // clear save
+        BLSave save = getBLSave();
+        if(save)
+            save.clear();
+        
+        // clear icons color
+        foreach (Transform iconTransform in icons.transform)
+        {
+            Image icon = iconTransform.gameObject.GetComponent<Image>();
+            if (icon)
+                icon.color = new Vector4(1,1,1,1);
+        }
+    }
+
+    public void closeBL()
+    {
+        mainScreen.SetActive(true);
+        blGame.SetActive(false);
+        
+        // clear save
+        BLSave save = getBLSave();
+        if(save)
+            save.clear();
+    }
+
+    public void makeChoise()
+    {
+        mainScreen.SetActive(true);
+        blGame.SetActive(false);
+    }
+
+    public void clickIcon(int ind)
+    {
+        // change icon's color
+        if (icons.transform.childCount > ind)
+        {
+            Image icon = icons.transform.GetChild(ind - 1).gameObject.GetComponent<Image>();
+            if (icon)
+                icon.color = new Vector4(1, 1, 1, 0.7f);
+        }
+        
+        // add save
+        BLSave save = getBLSave();
+        if(save)
+            save.makeChoise(ind);
+    }
+
+    private BLSave getBLSave()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player)
+            return player.GetComponent<BLSave>();
+        return null;
     }
 }
