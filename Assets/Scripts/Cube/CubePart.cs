b03855ece59b3f7bb9526d82d076f94e9071923c
old-fritz <komarov.99@yandex.ru>
2019-02-16 00:23:02 +0300

    Add rubik cube

---
+++ Assets/Scripts/Cube/CubePart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class CubePart : MonoBehaviour
+{
+
+	private Vector3 startPosition;
+	// Use this for initialization
+	void Start ()
+	{
+		startPosition = transform.localPosition;
+	}
+
+	public bool checkCorrect()
+	{
+		// check if start and current positions are equal
+		float epsilon = 0.0001f;
+		Vector3 difference = transform.localPosition - startPosition;
+		if (Math.Abs(difference.x) < epsilon && Math.Abs(difference.y) < epsilon && Math.Abs(difference.z) < epsilon)
+			return true;
+		return false;
+	}
+}
