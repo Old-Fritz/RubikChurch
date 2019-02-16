614284d6f8e194be1aa01c0ccc5a0e8398ea996c
old-fritz <komarov.99@yandex.ru>
2019-02-16 16:19:37 +0300

    Add flashlight

--- Assets/Scripts/Common/PlayerComps/Player.cs
+++ Assets/Scripts/Common/PlayerComps/Player.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Security.Permissions;
+using UnityEngine;
 
 namespace Common.PlayerComps
 {
     public class Player : MonoBehaviour
     {
+        [SerializeField] private GameObject flashLight;
+        
         public static GameObject main{get;protected set;}
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+                flashLight.SetActive(!flashLight.activeInHierarchy);
+        }
     
         void Start()
         {
