887a55a3fbfe7ac7eec5c8e0c29efa1da03f969c
old-fritz <komarov.99@yandex.ru>
2019-02-22 01:17:20 +0300

    change candles

---
+++ Assets/Scripts/Common/Properties/ClickActivate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickActivate : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+
+    public void onClick()
+    {
+        obj.SetActive(!obj.activeInHierarchy);
+    }
+}
